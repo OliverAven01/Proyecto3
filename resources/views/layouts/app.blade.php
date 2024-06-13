f3b707685c49cbe545e4de73ba67777fb2df9df1
Jeefry Javier Zamata Corimanya <jeefry.zamata@tecsup.edu.pe>
2024-06-13 01:06:45 -0500

    CreaciónPlantillaPrincipalyCompletandoVistas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,36 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="referrer" content="always">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+    <!-- Styles -->
+    <link href="/css/main.css" rel="stylesheet">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
+    <!-- Scripts -->
+    <script src="https://cdn.jsdelivr.net/npm/alpinejs" defer></script>
+</head>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
+<body>
+    <div x-data="{ sidebarOpen: false }" class="flex h-screen bg-gray-200 font-roboto">
+        @include('layouts.sidebar')
+        <div class="flex-1 flex flex-col overflow-hidden">
+            @include('layouts.header')
+
+            <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
+                <div class="container mx-auto px-6 py-8">
+                    @yield('content')
+                </div>
             </main>
         </div>
-    </body>
+    </div>
+</body>
+
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Chef_icon.svg/1500px-Chef_icon.svg.png">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
-    <link href="./CSS/estilos_mp.css" rel="stylesheet">
+    <link href="/css/home.css" rel="stylesheet">
+    <link href="/css/carousel.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <title>MY-GOOD-KITCHEN</title>
+
+    <title>POLLERIA MIGUELON I</title>
 </head>
 <body>
   <header>
@@ -16,11 +18,36 @@
           <div class="container d-flex justify-content-between flex-row">
             <a href="mainpage.html" class="navbar-brand d-flex align-items-center">
                 <i class="fas fa-bread-slice">&nbsp;&nbsp;</i>
-                <strong class="fa-sharp fa-solid fa-user-chef">My Good Kitchen</strong>
+                <strong class="fa-sharp fa-solid fa-user-chef">POLLERIA MIGUELON I</strong>
             </a>
             <a href="Nosotros.html" class="btn btn-dark btn-sm">Acerca de nosotros</a>
             <a href="#" class="btn btn-dark btn-sm">Blog</a>
-            <a href="#" class="btn btn-dark btn-sm">Unirse</a>
+            @if (Route::has('login'))
+                                @auth
+                                    <a
+                                        href="{{ url('/dashboard') }}"
+                                        class="rounded-md px-3 py-2 ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                    >
+                                        Dashboard
+                                    </a>
+                                @else
+                                    <a
+                                        href="{{ route('login') }}"
+                                        class="rounded-md px-3 py-2 ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                    >
+                                        Iniciar Sesión
+                                    </a>
+
+                                    @if (Route::has('register'))
+                                        <a
+                                            href="{{ route('register') }}"
+                                            class="rounded-md px-3 py-2 ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                        >
+                                        Registrar sesión
+                                        </a>
+                                    @endif
+                                @endauth
+                        @endif
           </div>
         </div>
     </header>
@@ -29,7 +56,7 @@
         <div class="fixed-top"><a href="#"><button></button></a></div>
         <section class="jumbotron text-center">
           <div class="container">
-            <h1 class="jumbotron-heading text-light encabezado">My Good Kitchen</h1>
+            <h1 class="jumbotron-heading text-light encabezado">Polleria Miguelon I</h1>
             <p class="subencabezado">Las mejores recetas y con la mejor comunidad sobre variedad de comida. ¡¡ A degustar!!.</p>
             <p>
                 <div class="form-outline">
@@ -234,7 +261,7 @@
                 <a href="https://www.twitter.com/" target="_blank"><i class="fa-brands fa-twitter-square"></i></a>
                 <a href="https://www.youtube.com/" target="_blank"><i class="fa-brands fa-youtube-square"></i></a>
             </div>
-            <p>© 2023 MyGoodKitchen. Derechos Reservados</p>
+            <p>© 2024 PolleriaMiguelonI - Derechos Reservados</p>
         </p>
     </div>
     </footer>
